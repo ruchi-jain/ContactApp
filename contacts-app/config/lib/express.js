13d02acf1a4e41c9cd6ec9872f03366c51d4d344
Ruchi Goel <ruchi.goel@outlook.com>
2016-07-10 16:30:09 -0700

    initial commit

---
+++ contacts-app/config/lib/express.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Ruchi on 7/10/16.
+ */
+'use strict';
+
+var express = require('express');  //3rd party library
+
+module.exports.init = function(){
+    var app = express();
+
+    return app;
+};
